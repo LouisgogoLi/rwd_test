bcd19990076bcd6f7bd61483781c7e923bee43b9
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2021-12-06 00:30:29 +0800

    新部屬RWD_TEST

---
+++ src/RWDApp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Header />
+    <Article />
+    <Aside />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Article from "@/components/Article.vue";
+import Aside from "@/components/Aside.vue";
+import Main from "@/components/Main.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Article,
+    Aside,
+    Main,
+    Footer,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Microsoft JhengHei", "Heiti TC", "sans-serif";
+}
+img {
+  display: block;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+</style>
