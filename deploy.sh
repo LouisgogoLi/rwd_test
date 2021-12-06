6482ed400b07923592a8cf1ac506f271ed331018
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2021-12-06 09:14:36 +0800

    修正deploy備註

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 將 dist資料夾中的內容推送至遠端 hexWeek6Demo的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+# 將 dist資料夾中的內容推送至遠端的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 git push -f https://github.com/LouisgogoLi/rwd_test.git master:gh-pages
 cd -
 
